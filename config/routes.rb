5ea80d6425b3e9d9a5809dc2bc5a879561e964a3
Manuel Venegas <manuelalexander08@gmail.com>
2018-10-08 14:42:43 -0500

    Se puede registrar, logearse y finalizar seccion

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	
+ devise_for :usuarios
  resources :tareas
  #	get 'tareas', to: 'tareas#index'
  #	get 'tareas/new', to: 'tareas#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181002203354) do
+ActiveRecord::Schema.define(version: 20181008185121) do
 
   create_table "tareas", force: :cascade do |t|
     t.string "titulo"
     t.text "descripcion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "usuarios", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_usuarios_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
   end
 
 end
